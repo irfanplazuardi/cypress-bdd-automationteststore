319d5c753711267df8bb23a9bd97baf6fe2b317c
Irfan <irfanplazuardi@gmail.com>
2023-09-04 12:14:09 +0800

    register

--- cypress/e2e/features/register.feature
+++ cypress/e2e/features/register.feature
@@ -1,9 +1,51 @@
 Feature: Automation Test Store Register
 
-    Scenario: Register with valid account
+    Scenario: Register with valid data
         Given I access the automation test store homepage
         When I click on the login or register button
         And I click continue in register account box
         And I fill in the account details
         And I click continue
-        Then New account will be created
+        Then New account will be created when all field is inputted
+
+    Scenario: Register with empty data
+        Given I access the automation test store homepage
+        When I click on the login or register button
+        And I click continue in register account box
+        And I click continue
+        Then It will show error message to fill in required fields
+
+    Scenario: Register when only fill no mandatory fields
+        Given I access the automation test store homepage
+        When I click on the login or register button
+        And I click continue in register account box
+        And I fill in the account details when only fill no mandatory fields
+        And I click continue
+        Then It will show error message to fill in required fields
+
+    Scenario: Register when only fill mandatory fields
+        Given I access the automation test store homepage
+        When I click on the login or register button
+        And I click continue in register account box
+        And I fill in the account details when only fill mandatory fields
+        And I click continue
+        Then New account will be created when only mandatory field is inputted
+
+    Scenario: Register with existing email account
+        Given I access the automation test store homepage
+        When I click on the login or register button
+        And I click continue in register account box
+        And I fill in the account details with existing email account
+        And I click continue
+        Then It will show error message Error: E-Mail Address is already registered!
+
+    Scenario: Register with existing username account
+        Given I access the automation test store homepage
+        When I click on the login or register button
+        And I click continue in register account box
+        And I fill in the account details with existing username account
+        And I click continue
+        Then It will show error message This login name is not available. Try different login name!
+
+
+
--- cypress/support/step_definitions/register.js
+++ cypress/support/step_definitions/register.js
@@ -1,50 +1,193 @@
 /// <reference types="Cypress" />
-import { Before, Given, When, And, Then } from "cypress-cucumber-preprocessor/steps"; 
+import { Before, Given, When, And, Then } from "cypress-cucumber-preprocessor/steps";
 import { faker } from '@faker-js/faker'
 
-Before(() => {
-    cy.visit("/")
-})
+let data = require("../../fixtures/account.json");
+
+let globalFirstName1 = faker.person.firstName()
+let globalFirstName2 = faker.person.firstName()
+
 
 Given("I access the automation test store homepage", () => {
-   cy.get('img').should('have.attr', 'title', 'Automation Test Store')
-}) 
+    cy.visit("/")
+    cy.get('img').should('have.attr', 'title', 'Automation Test Store')
+})
 
 When("I click on the login or register button", () => {
     cy.get('#customernav').click()
 })
 
 And("I click continue in register account box", () => {
-    cy.contains("button","Continue").click()
+    cy.contains("button", "Continue").click()
 })
 
 And("I fill in the account details", () => {
-    cy.get('#AccountFrm_firstname').type(faker.person.firstName())
-    cy.get('#AccountFrm_lastname').type(faker.person.lastName())
-    cy.get("#AccountFrm_email").type(faker.internet.email())
-    cy.get("#AccountFrm_telephone").type(faker.phone.number('08##########'))
-    cy.get('#AccountFrm_fax').type(faker.phone.number('######'))
-    cy.get('#AccountFrm_company').type(faker.company.name("PT. "))
-    cy.get("#AccountFrm_address_1").type(faker.location.streetAddress())
-    cy.get("#AccountFrm_address_2").type(faker.location.streetAddress())
+    let lastName = faker.person.lastName()
+    let email = faker.internet.email()
+    let telephone = faker.phone.number('08##########')
+    let fax = faker.phone.number('######')
+    let company = faker.company.name("PT. ")
+    let address1 = faker.location.streetAddress()
+    let address2 = faker.location.streetAddress()
+    let city = faker.location.city()
+    let postcode = faker.location.zipCode()
+    let loginName = globalFirstName1 + faker.number.int(100)
+
+    cy.get('#AccountFrm_firstname').type(globalFirstName1)
+    cy.get('#AccountFrm_lastname').type(lastName)
+    cy.get("#AccountFrm_email").type(email)
+    cy.get("#AccountFrm_telephone").type(telephone)
+    cy.get('#AccountFrm_fax').type(fax)
+    cy.get('#AccountFrm_company').type(company)
+    cy.get("#AccountFrm_address_1").type(address1)
+    cy.get("#AccountFrm_address_2").type(address2)
     cy.get("#AccountFrm_country_id").select("Indonesia")
-    cy.get('#AccountFrm_city').type(faker.location.city())
+    cy.get('#AccountFrm_city').type(city)
     cy.get("#AccountFrm_zone_id").select("Nusa Tenggara Barat")
-    cy.get("#AccountFrm_postcode").type(faker.location.zipCode())
-    cy.get("#AccountFrm_loginname").type(faker.internet.userName())
-    cy.get('#AccountFrm_password').type("0qwerty'")
-    cy.get('#AccountFrm_confirm').type("0qwerty'")
+    cy.get("#AccountFrm_postcode").type(postcode)
+    cy.get("#AccountFrm_loginname").type(loginName)
+    cy.get('#AccountFrm_password').type(data.password, {log: false})
+    cy.get('#AccountFrm_confirm').type(data.password, {log: false})
     cy.get("#AccountFrm_newsletter0").click()
     cy.get('#AccountFrm_agree').click()
 })
 
-And ("I click continue", () => {
-    cy.contains("button","Continue").click()
+And("I fill in the account details when only fill no mandatory fields", () => {
+    let telephone = faker.phone.number('08##########')
+    let fax = faker.phone.number('######')
+    let company = faker.company.name("PT. ")
+    let address2 = faker.location.streetAddress()
+
+    cy.get("#AccountFrm_telephone").type(telephone)
+    cy.get('#AccountFrm_fax').type(fax)
+    cy.get('#AccountFrm_company').type(company)
+    cy.get("#AccountFrm_address_2").type(address2)
+    cy.get("#AccountFrm_newsletter0").click()
+    cy.get('#AccountFrm_agree').click()
 })
 
-Then ("New account will be created", () => {
-    cy.get("span.maintext").should("contain", "Your Account Has Been Created!")
-    cy.contains("a","Continue").click()
-    cy.get(".menu_text").should("contain", "Welcome back")
+And("I fill in the account details when only fill mandatory fields", () => {
+    let lastName = faker.person.lastName()
+    let email = faker.internet.email()
+    let address1 = faker.location.streetAddress()
+    let city = faker.location.city()
+    let postcode = faker.location.zipCode()
+    let loginName = globalFirstName2 + faker.number.int(100)
+
+    cy.get('#AccountFrm_firstname').type(globalFirstName2)
+    cy.get('#AccountFrm_lastname').type(lastName)
+    cy.get("#AccountFrm_email").type(email)
+    cy.get("#AccountFrm_address_1").type(address1)
+    cy.get("#AccountFrm_country_id").select("Indonesia")
+    cy.get('#AccountFrm_city').type(city)
+    cy.get("#AccountFrm_zone_id").select("Nusa Tenggara Barat")
+    cy.get("#AccountFrm_postcode").type(postcode)
+    cy.get("#AccountFrm_loginname").type(loginName)
+    cy.get('#AccountFrm_password').type(data.password, {log: false})
+    cy.get('#AccountFrm_confirm').type(data.password, {log: false})
+    cy.get("#AccountFrm_newsletter0").click()
+    cy.get('#AccountFrm_agree').click()
 })
+
+And("I fill in the account details with existing email account", () => {
+    let firstName = faker.person.firstName()
+    let lastName = faker.person.lastName()
+    let email = faker.internet.email()
+    let telephone = faker.phone.number('08##########')
+    let fax = faker.phone.number('######')
+    let company = faker.company.name("PT. ")
+    let address1 = faker.location.streetAddress()
+    let address2 = faker.location.streetAddress()
+    let city = faker.location.city()
+    let postcode = faker.location.zipCode()
+    let loginName = firstName + faker.number.int(100)
+
+    cy.get('#AccountFrm_firstname').type(firstName)
+    cy.get('#AccountFrm_lastname').type(lastName)
+    cy.get("#AccountFrm_email").type(email = data.email,{log: false})
+    cy.get("#AccountFrm_telephone").type(telephone)
+    cy.get('#AccountFrm_fax').type(fax)
+    cy.get('#AccountFrm_company').type(company)
+    cy.get("#AccountFrm_address_1").type(address1)
+    cy.get("#AccountFrm_address_2").type(address2)
+    cy.get("#AccountFrm_country_id").select("Indonesia")
+    cy.get('#AccountFrm_city').type(city)
+    cy.get("#AccountFrm_zone_id").select("Nusa Tenggara Barat")
+    cy.get("#AccountFrm_postcode").type(postcode)
+    cy.get("#AccountFrm_loginname").type(loginName)
+    cy.get('#AccountFrm_password').type(data.password, {log: false})
+    cy.get('#AccountFrm_confirm').type(data.password, {log: false})
+    cy.get("#AccountFrm_newsletter0").click()
+    cy.get('#AccountFrm_agree').click()
+})
+
+And("I fill in the account details with existing username account", () => {
+    let firstName = faker.person.firstName()
+    let lastName = faker.person.lastName()
+    let email = faker.internet.email()
+    let telephone = faker.phone.number('08##########')
+    let fax = faker.phone.number('######')
+    let company = faker.company.name("PT. ")
+    let address1 = faker.location.streetAddress()
+    let address2 = faker.location.streetAddress()
+    let city = faker.location.city()
+    let postcode = faker.location.zipCode()
+    let loginName = firstName + faker.number.int(100)
+
+    cy.get('#AccountFrm_firstname').type(firstName)
+    cy.get('#AccountFrm_lastname').type(lastName)
+    cy.get("#AccountFrm_email").type(email)
+    cy.get("#AccountFrm_telephone").type(telephone)
+    cy.get('#AccountFrm_fax').type(fax)
+    cy.get('#AccountFrm_company').type(company)
+    cy.get("#AccountFrm_address_1").type(address1)
+    cy.get("#AccountFrm_address_2").type(address2)
+    cy.get("#AccountFrm_country_id").select("Indonesia")
+    cy.get('#AccountFrm_city').type(city)
+    cy.get("#AccountFrm_zone_id").select("Nusa Tenggara Barat")
+    cy.get("#AccountFrm_postcode").type(postcode)
+    cy.get("#AccountFrm_loginname").type(loginName = data.name, {log: false})
+    cy.get('#AccountFrm_password').type(data.password, {log: false})
+    cy.get('#AccountFrm_confirm').type(data.password, {log: false})
+    cy.get("#AccountFrm_newsletter0").click()
+    cy.get('#AccountFrm_agree').click()
+})
+
+
+And("I click continue", () => {
+    cy.contains("button", "Continue").click()
+})
+
+Then("New account will be created when all field is inputted", () => {
+    cy.get("span.maintext").should("contain", "Your Account Has Been Created!")
+    cy.contains("a", "Continue").click()
+    cy.get(".menu_text").should("contain", "Welcome back " + globalFirstName1)
+})
+
+Then("New account will be created when only mandatory field is inputted", () => {
+    cy.get("span.maintext").should("contain", "Your Account Has Been Created!")
+    cy.contains("a", "Continue").click()
+    cy.get(".menu_text").should("contain", "Welcome back " + globalFirstName2)
+})
+
+Then("It will show error message to fill in required fields", () => {
+    cy.get('.help-block').contains("First Name must be between 1 and 32 characters!")
+    cy.get('.help-block').contains("Last Name must be between 1 and 32 characters!")
+    cy.get('.help-block').contains("Email Address does not appear to be valid!")
+    cy.get('.help-block').contains("Address 1 must be between 3 and 128 characters!")
+    cy.get('.help-block').contains("City must be between 3 and 128 characters!")
+    cy.get('.help-block').contains("Please select a region / state!")
+    cy.get('.help-block').contains("Zip/postal code must be between 3 and 10 characters!")
+    cy.get('.help-block').contains("Login name must be alphanumeric only and between 5 and 64 characters!")
+    cy.get('.help-block').contains("Password must be between 4 and 20 characters!")
+})
+
+Then("It will show error message Error: E-Mail Address is already registered!", () => {
+    cy.get(".alert").contains('Error: E-Mail Address is already registered!')
+})
+
+Then("It will show error message This login name is not available. Try different login name!", () => {
+    cy.get(".alert").contains('This login name is not available. Try different login name!')
+})
+
 
